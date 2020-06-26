9aeefcfb30ef0cd5e9ed175ed1de374177cf8bac
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-06-26 17:03:22 -0300

    setup home work

--- run.sh
+++ run.sh
@@ -4,13 +4,16 @@ echo "abrindo github"
 google-chrome https://www.google.com > /dev/null 2>&1 &
 
 echo "abrindo editor"
-code -r . > /dev/null 2>&1 &
+code > /dev/null 2>&1 &
 
 echo "abrindo whatsapp"
 google-chrome https://web.whatsapp.com/ > /dev/null 2>&1 &
 
-echo "abrindo skype"
-skypeforlinux > /dev/null 2>&1 &
+# echo "abrindo skype"
+# skypeforlinux > /dev/null 2>&1 &
 
 echo "iniciando docker"
 docker start postgresql && docker start mysql-db > /dev/null 2>&1 &
+
+echo "update system"
+sudo apt update && sudo apt upgrade -y && exit > /dev/null 2>&1 &
